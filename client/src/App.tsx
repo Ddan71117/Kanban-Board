c39c0665ec4ff934bb855814161d1ce752dd5e0d
Ddan71117 <137979308+Ddan71117@users.noreply.github.com>
2024-12-26 22:26:03 -0700

    Finished code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,24 @@
-import { Outlet } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import InactivityLogout from "./hooks/InactivityLogout";
+import AuthService from "./utils/auth";
 
 function App() {
+  const timeoutDuration = 300000;
+  const logout = () => {
+    AuthService.logout();
+  };
+
+  InactivityLogout(timeoutDuration, logout);
 
   return (
-    <div className='container'>
+    <div className="container">
       <Navbar />
       <main>
         <Outlet />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
